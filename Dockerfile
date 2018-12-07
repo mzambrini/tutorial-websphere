5473ebb15732e34c29081c4e1f160782bb4019a5
Massimiliano Zambrini <mzambrini@gmail.com>
2018-12-07 09:37:17 +0100

    Creazione progetto

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM brainmaxz/was-scripting:0.6.5 as Create
+LABEL maintainer="m.zambrini@informatica.aci.it"
+
+ENV JYTHON_FILE_NAME was_script.py
+COPY install.json /config/install.json
+RUN /work/main
+
+FROM ibmcom/websphere-traditional:8.5.5.13-profile
+
+ARG ADMIN_PASSWORD=iniziale
+
+COPY /scripts /scripts
+COPY /artifacts  /artifacts
+
+COPY --from=Create /work/result/was_script.py /scripts
+
+USER 0
+
+RUN chown -R $USER:$GROUP /scripts && chmod -R a+x /scripts/*.sh 
+ 
+USER $USER
+
+RUN echo $ADMIN_PASSWORD > /tmp/PASSWORD
+RUN set -x \
+&& /scripts/apply-configuration.sh was_script.py
+
+CMD ["/work/start_server"]
---
+++ scripts/apply-configuration.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#####################################################################################
+#                                                                                   #
+#  Esegue tutti gli script di configurazione in cascata                             #
+#                                                                                   #
+#####################################################################################
+
+PROFILE_NAME=${PROFILE_NAME:-"AppSrv01"}
+
+scriptFile="/scripts/$1"
+
+test -r "$scriptFile" || { echo "Script file $scriptFile not found. Aborting"; exit 1;}
+echo "Apply script $1"
+cat "$scriptFile"
+
+/opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/wsadmin.sh \
+     -lang jython \
+     -conntype NONE \
+     -f "$scriptFile"
+
+
